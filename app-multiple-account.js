3b990c1c867279173a6b7bd1306ab6fac68171c7
veezo2007pk <36745222+veezo2007pk@users.noreply.github.com>
2023-01-02 10:12:11 +0500

    Update app-multiple-account.js

--- app-multiple-account.js
+++ app-multiple-account.js
@@ -92,23 +92,24 @@ const pool = mysql.createPool(
 
 app.get("/get_instance", function (req, res, next) {
   pool.getConnection(function (err, connection) {
+    if (err) throw error;
+
     // Use the connection
     connection.query(
       "SELECT * FROM tblinstance ORDER BY intInstanceCode desc",
       function (error, results, fields) {
-        // And done with the connection.
-        connection.release();
-
-        // Handle error after the release.
         if (error) throw error;
+
         return res.status(200).json([
           {
             rows,
           },
         ]);
+
         // Don't use the connection here, it has been returned to the pool.
       }
     );
+    connection.release();
   });
   // connection.query(
   //   "SELECT * FROM tblinstance ORDER BY intInstanceCode desc",
